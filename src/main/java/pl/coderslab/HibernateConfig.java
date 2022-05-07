c6bad473f537fa1379a8f550f0a91a2d7d990a0a
edu <edu@mail.pl>
2022-05-07 11:52:08 +0200

    configure hibernate and db

---
+++ src/main/java/pl/coderslab/HibernateConfig.java
@@ -0,0 +1,27 @@
+package pl.coderslab;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean
+                = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("hibernateExample");
+        return entityManagerFactoryBean;
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager =
+                new JpaTransactionManager(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+}
